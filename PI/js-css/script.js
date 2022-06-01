2f6c7451ed2686efc14a9840bf15e782a7ebe2da
Rafael M <99418707+Mr-R4F@users.noreply.github.com>
2022-06-01 02:49:52 -0300

    Delete PI directory

--- PI/js-css/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $("button:first").click(function () {
-        $("form").addClass('was-validated');
-        $("input:nth-child(n + 1), input:focus").css({'outline':'none', 'box-shadow':'none'});
-    });
-});
